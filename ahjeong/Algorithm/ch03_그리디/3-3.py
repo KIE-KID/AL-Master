ed55ef4266f95a464d4ca35b9f3b492ab962e133
Ahjeong Park <qkrdkwjd59@naver.com>
2021-07-25 19:36:27 +0900

    Ch03 그리디 실전문제 : 3-3 숫자 카드 게임 풀이

--- ahjeong/Algorithm/ch03_그리디/3-3.py
+++ ahjeong/Algorithm/ch03_그리디/3-3.py
@@ -1,3 +1,7 @@
+# 난이도 (하)
+# 풀이 시간 : 3분
+# 아이디어 : 각 행마다 가장 작은 수를 찾은 뒤에 그 수 중에서 가장 큰 수
+
 r, c = map(int, input().split())
 data = []
 min_list = []
@@ -6,5 +10,3 @@ for i in range(r):
     min_list.append(min(data[i]))
 
 result = max(min_list)
-
-print(result)
