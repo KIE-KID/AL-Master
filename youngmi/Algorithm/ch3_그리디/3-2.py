f8f49141d49f086f6850d476ce97e73650df83e8
Youngmi Park <yommi1121@naver.com>
2021-07-28 03:24:12 +0900

    ch8 dynamic programming fibonacci function

--- youngmi/Algorithm/ch3_그리디/3-2.py
+++ youngmi/Algorithm/ch3_그리디/3-2.py
@@ -43,7 +43,7 @@ print(result1) # 답안 출력
 '''
 좀 더 효율적인 방법으로 해결하기
 정렬과 가장큰수, 다음으로 큰수를 사용하는 것은 동일하다.
-가장큰수 k개 그 다음으로 큰수 1개 (k+1)이 int(m/(k+1)) 쌍 만큼 존재하고 
+가장큰수 k개 그 다음으로 큰수 1개 (k+1)이 int(m/(k+1)) 쌍 만큼 존재하고
 나머지가 있는경우 그 수만큼 가장 큰수를 더해주면된다.
 '''
 
@@ -54,3 +54,29 @@ result2 += int(m/(k+1)) * second_num
 result2 += (m % (k+1)) * first_num
 
 print(result2)
+
+
+'''
+210724 복습
+문제 아이디어
+입력받은 데이터를 큰 순서대로 정렬하기
+가장 큰 숫자와 그 다음으로 큰 숫자를 변수로 기록
+가장 큰 숫자는 k번 그 다음으로 큰 숫자는 1번 이걸 반복해서 m번으로 반들면 된다. ex) (1112)(1112)(11)
+'''
+n, m, k =  map(int, input().split()) # 배열크기 n, 숫자가 더해지는 횟수 m, 연속해서 더해질 수 있는 횟수 k
+data = list(map(int,input().split())) # 자연수 n개 - 배열로 저장
+
+data.sort() # 반환하는 리스트x
+first = data[-1]
+second = data[-2]
+
+# 가장 큰 수가 더해지는 횟수
+
+count = (m // (k+1)) * k
+count += m % (k+1)
+
+result = 0
+result += count * first
+result += (m-count) * second
+
+print(result)
